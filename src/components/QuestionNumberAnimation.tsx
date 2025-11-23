a3ec53d7a8f97d227886b747e01ca2a5223bf8fd
sd-codebase <krishna.dhas021815@gmail.com>
2025-11-23 22:17:41 +0530

    Update QuestionNumberAnimation.tsx

--- src/components/QuestionNumberAnimation.tsx
+++ src/components/QuestionNumberAnimation.tsx
@@ -22,7 +22,10 @@ export default function QuestionNumberAnimation({
         }
         .question-number-animate-container {
           position: fixed;
-          inset: 0;
+          left: 0;
+          right: 0;
+          bottom: 0;
+          top: -30%;
           display: flex;
           align-items: center;
           justify-content: center;
